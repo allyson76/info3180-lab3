5204f9a314eedd5ebc05d585db73b77f86f1b9a9
allyson76 <32620919+allyson76@users.noreply.github.com>
2018-02-18 20:16:47 -0500

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'givemeaccess'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '2525'
-app.config['MAIL_USERNAME'] = 'ee4afaefc31ed1'
-app.config['MAIL_PASSWORD'] = 'b1d908539fa0de'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 mail = Mail(app)
 
 from app import views
